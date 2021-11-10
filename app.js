11326cc935bdbae04629111e6b8e61b254b30699
jcsfpc <joanaparentedacosta@gmail.com>
2021-11-10 17:53:01 +0000

    lab with Josh

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +21,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    // console.log('Beers from the database: ', beersFromApi)
+    res.render(__dirname + '/views/beers.hbs', {beers : beersFromApi})
+  })
+  .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeersFromApi => {
+    console.log(randomBeersFromApi)
+    res.render(__dirname + '/views/random-beer.hbs', {randombeers : randomBeersFromApi})
+  })
+  .catch(error => console.log(error));
+
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
